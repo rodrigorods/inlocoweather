a24b13bf4f3a0fcefdd98519cc007a28f1d4e254
rodrigorods <rodrigo.otavio89@gmail.com>
2016-10-20 01:48:37 -0200

    cityweather implementing parcelable

--- InLocoWeather/app/src/main/java/com/teste/rodrigo/inlocoweather/model/CityWeather.java
+++ InLocoWeather/app/src/main/java/com/teste/rodrigo/inlocoweather/model/CityWeather.java
@@ -1,6 +1,9 @@
 package com.teste.rodrigo.inlocoweather.model;
 
-public class CityWeather {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CityWeather implements Parcelable {
 
     private String cityName;
     private double minimumTemperature, maximumTemperature;
@@ -28,4 +31,37 @@ public class CityWeather {
     public String getWeatherDescription() {
         return weatherDescription;
     }
+
+    ///////////////////////PARCELABLE CONTENT///////////////////////////
+
+    public CityWeather(Parcel in){
+        this.cityName = in.readString();
+        this.minimumTemperature = in.readDouble();
+        this.maximumTemperature = in.readDouble();
+        this.weatherDescription = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.cityName);
+        dest.writeDouble(this.minimumTemperature);
+        dest.writeDouble(this.maximumTemperature);
+        dest.writeString(this.weatherDescription);
+    }
+
+    public static final Parcelable.Creator<CityWeather> CREATOR = new Parcelable.Creator<CityWeather>() {
+        @Override
+        public CityWeather createFromParcel(Parcel in) {
+            return new CityWeather(in);
+        }
+        @Override
+        public CityWeather[] newArray(int size) {
+            return new CityWeather[size];
+        }
+    };
 }
