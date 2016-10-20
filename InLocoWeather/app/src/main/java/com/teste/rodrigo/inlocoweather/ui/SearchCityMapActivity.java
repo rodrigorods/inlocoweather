612ac150a437368bd562e85b80c62d8fb5dcb5a6
rodrigorods <rodrigo.otavio89@gmail.com>
2016-10-20 01:49:53 -0200

    layout update

--- InLocoWeather/app/src/main/java/com/teste/rodrigo/inlocoweather/ui/SearchCityMapActivity.java
+++ InLocoWeather/app/src/main/java/com/teste/rodrigo/inlocoweather/ui/SearchCityMapActivity.java
@@ -1,13 +1,17 @@
 package com.teste.rodrigo.inlocoweather.ui;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.teste.rodrigo.inlocoweather.R;
 import com.teste.rodrigo.inlocoweather.model.CityWeather;
@@ -17,33 +21,42 @@ import com.teste.rodrigo.inlocoweather.mvp.search_cities.SearchCitiesPresenterIm
 
 import java.util.List;
 
-public class SearchCityMapActivity extends FragmentActivity implements OnMapReadyCallback, ISearchCitiesView {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SearchCityMapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener, ISearchCitiesView {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.search_cities_weather_btn)
+    Button mSearchSurroundingsBtn;
 
     private ISearchCitiesPresenter mPresenter;
     private GoogleMap mMap;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_city_map);
+        ButterKnife.bind(this);
+
+        mToolbar.setTitle(R.string.title_activity_search_city_map);
+        setSupportActionBar(mToolbar);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        LatLng sydney = new LatLng(-34, 151);
         this.mPresenter = new SearchCitiesPresenterImp(this, getResources());
-        this.mPresenter.searchSurroundingCities(sydney);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMarkerClickListener(this);
     }
 
     @Override
@@ -52,7 +65,32 @@ public class SearchCityMapActivity extends FragmentActivity implements OnMapRead
     }
 
     @Override
-    public void onCitiesError() {
+    public void onCitiesLoadError() {
 
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        removeMarker(this.marker);
+
+        mSearchSurroundingsBtn.setVisibility(View.VISIBLE);
+
+        marker = mMap.addMarker(new MarkerOptions().position(latLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//        this.mPresenter.searchSurroundingCities(latLng);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        removeMarker(marker);
+        mSearchSurroundingsBtn.setVisibility(View.GONE);
+        return true;
+    }
+
+    private void removeMarker(Marker marker){
+        if (marker != null) {
+            marker.remove();
+            this.marker = null;
+        }
     }
 }
